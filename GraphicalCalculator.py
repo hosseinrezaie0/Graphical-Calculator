e789a2553ea1d055980297c23d85d304e9758637
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2023-08-29 12:04:10 +0330

    window created

--- GraphicalCalculator.py
+++ GraphicalCalculator.py
@@ -1,13 +1,21 @@
 from tkinter import *
 
-#------------------------COLORS--------------
+#------------------------------COLORS------------------------
 NAVY_BLUE = "#445069"
 GRAY = "#9E9FA5"
 LIGHT_BLUE = "#C5DFF8"
 
 
 
+#-----------------------------UI design----------------------
+calculator = Tk()
+calculator.title("Calculator")
+calculator.geometry("400x500")
 
 
+
+
+
+calculator.mainloop()
 
 
